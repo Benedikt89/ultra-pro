0e02ae92abeab1a7d349cb050b9fcb44b1cdb2ec
Benedikt89 <53091932+Benedikt89@users.noreply.github.com>
2022-10-20 13:31:26 +0200

    gh-pages hash-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {Suspense, useEffect, useState} from 'react';
 import {Provider} from "react-redux";
-import {BrowserRouter as Router,} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 
 import store from "store/store";
 import Keys from 'constants/appKeys';
